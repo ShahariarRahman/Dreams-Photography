d5ebe13bb0970580d337069a3f5d19932cd0e062
ShahariarRahman <shahariarrahman98@gmail.com>
2022-04-18 21:21:45 +0600

    Homepage modified

--- src/Pages/Home/Title/Title.js
+++ src/Pages/Home/Title/Title.js
@@ -7,7 +7,9 @@ const Title = () => {
                 <h1 className='text-3xl md:text-4xl lg:text-5xl text-gray-700 font-bold p-3 mt-3'>Welcome to <p className='text-indigo-700'>Dreams Photography !</p></h1>
             </div>
             <div>
-                <h1 className='text-xl text-gray-700 font-light p-3'>I am</h1>
+                <h1 className='text-xl text-gray-700 font-light p-3'>I am a professional photographer.
+                    I cover wedding ceremony. I take photo as well as cinematic wedding video. I have all professional Photography instrument to make your marrage photo awesome. If you want to hire me choose a service and send me details. Thank you for visiting my website. Happy marrage life and make your wedding unforgettable and Special.
+                </h1>
             </div>
         </div >
     );
